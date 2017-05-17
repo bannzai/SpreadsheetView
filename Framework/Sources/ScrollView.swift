d00d6d976e30daa5e7b86796c4d903add60c82a8
kishikawa katsumi <kishikawakatsumi@mac.com>
2017-05-17 21:00:13 +0900

    Fix a view on the cell cannot receive touch events

--- Framework/Sources/ScrollView.swift
+++ Framework/Sources/ScrollView.swift
@@ -75,6 +75,11 @@ final class ScrollView: UIScrollView, UIGestureRecognizerDelegate {
             return
         }
         touchesBegan?(touches, event)
+        next?.touchesBegan(touches, with: event)
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        next?.touchesMoved(touches, with: event)
     }
 
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -82,6 +87,7 @@ final class ScrollView: UIScrollView, UIGestureRecognizerDelegate {
             return
         }
         touchesEnded?(touches, event)
+        next?.touchesEnded(touches, with: event)
     }
 
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -89,5 +95,6 @@ final class ScrollView: UIScrollView, UIGestureRecognizerDelegate {
             return
         }
         touchesCancelled?(touches, event)
+        next?.touchesCancelled(touches, with: event)
     }
 }
