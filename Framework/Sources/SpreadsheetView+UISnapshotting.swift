ebdddf535a08d1044bf5719d5bc77a5701be3cb3
Adam Nemecek <adamnemecek@gmail.com>
2017-08-01 14:49:04 -0700

    * refactored resizableSnapshotView

--- Framework/Sources/SpreadsheetView+UISnapshotting.swift
+++ Framework/Sources/SpreadsheetView+UISnapshotting.swift
@@ -8,25 +8,37 @@
 
 import UIKit
 
+extension CGRect {
+    func offset(by delta: CGPoint) -> CGRect {
+        return offsetBy(dx: delta.x, dy: delta.y)
+    }
+}
+
+extension CGPoint {
+    prefix static func -(point: CGPoint) -> CGPoint {
+        return CGPoint(x: -point.x, y: -point.y)
+    }
+}
+
 extension SpreadsheetView {
     public override func resizableSnapshotView(from rect: CGRect, afterScreenUpdates afterUpdates: Bool, withCapInsets capInsets: UIEdgeInsets) -> UIView? {
         if cornerView.frame.intersects(cornerView.convert(rect, to: self)) {
-            return cornerView.resizableSnapshotView(from: rect.offsetBy(dx: -cornerView.frame.origin.x, dy: -cornerView.frame.origin.y),
+            return cornerView.resizableSnapshotView(from: rect.offset(by: -cornerView.frame.origin),
                                                     afterScreenUpdates: afterUpdates,
                                                     withCapInsets: capInsets)
         }
         if columnHeaderView.frame.intersects(columnHeaderView.convert(rect, to: self)) {
-            return columnHeaderView.resizableSnapshotView(from: rect.offsetBy(dx: -columnHeaderView.frame.origin.x, dy: -columnHeaderView.frame.origin.y),
+            return columnHeaderView.resizableSnapshotView(from: rect.offset(by: -columnHeaderView.frame.origin),
                                                           afterScreenUpdates: afterUpdates,
                                                           withCapInsets: capInsets)
         }
         if rowHeaderView.frame.intersects(rowHeaderView.convert(rect, to: self)) {
-            return rowHeaderView.resizableSnapshotView(from: rect.offsetBy(dx: -rowHeaderView.frame.origin.x, dy: -rowHeaderView.frame.origin.y),
+            return rowHeaderView.resizableSnapshotView(from: rect.offset(by: -rowHeaderView.frame.origin),
                                                        afterScreenUpdates: afterUpdates,
                                                        withCapInsets: capInsets)
         }
         if tableView.frame.intersects(tableView.convert(rect, to: self)) {
-            return tableView.resizableSnapshotView(from: rect.offsetBy(dx: -tableView.frame.origin.x, dy: -tableView.frame.origin.y),
+            return tableView.resizableSnapshotView(from: rect.offset(by: -tableView.frame.origin),
                                                    afterScreenUpdates: afterUpdates,
                                                    withCapInsets: capInsets)
         }
