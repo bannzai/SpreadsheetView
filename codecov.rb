5f587fe27709d8fee3238f7c03790700620e6b94
bannzai <kingkonog999yhirose@gmail.com>
2020-10-10 14:49:16 +0900

    :memo: add codecov.rb

---
+++ codecov.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+def build_coverage_reports()
+  project_name = 'SpreadsheetView'
+  profdata = Dir.glob(File.join('build', '/**/Coverage.profdata')).first
+  Dir.glob(File.join('build', "/**/#{project_name}")) do |target|
+    output = `xcrun llvm-cov report -instr-profile "#{profdata}" "#{target}" -arch=x86_64`
+    if $?.success?
+      puts output
+      `xcrun llvm-cov show -instr-profile "#{profdata}" "#{target}" -arch=x86_64 -use-color=0 > coverage.txt`
+      break
+    end
+  end
+end
+
+build_coverage_reports()
