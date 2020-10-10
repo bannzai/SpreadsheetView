b57b8e5b327dc1b4056ef9f4f6581fda91ac5f09
bannzai <kingkonog999yhirose@gmail.com>
2020-10-10 15:01:57 +0900

    :memo: missing path

--- scripts/codecov.rb
+++ scripts/codecov.rb
@@ -7,7 +7,7 @@ def build_coverage_reports()
     output = `xcrun llvm-cov report -instr-profile "#{profdata}" "#{target}" -arch=x86_64`
     if $?.success?
       puts output
-      `xcrun llvm-cov show -instr-profile "#{profdata}" "#{target}" -arch=x86_64 -use-color=0 > ../coverage.txt`
+      `xcrun llvm-cov show -instr-profile "#{profdata}" "#{target}" -arch=x86_64 -use-color=0 > coverage.txt`
       break
     end
   end
